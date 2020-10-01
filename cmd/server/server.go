2ec72e9e964828d381c06228a93d4a58a528a1dd
Vo Lam Nhat Khuong <volamnhatkhuong@gmail.com>
2020-10-01 17:00:18 +0200

    working server sending dumb json

---
+++ cmd/server/server.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"go-rille/internal/json"
+	"log"
+	"net/http"
+)
+
+type person struct {
+	Age    int
+	Name   string
+	Prenom string
+}
+
+func tempHandler(w http.ResponseWriter, r *http.Request) {
+	p := person{Age: 12, Prenom: "Glenn", Name: "Louarn"}
+	json.WriteJSON(w, p)
+}
+
+func main() {
+	http.HandleFunc("/temp", tempHandler)
+	log.Fatal(http.ListenAndServe(":8888", nil))
+}
--- internal/json/json.go
+++ internal/json/json.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 )
 
+// WriteJSON : write a json to the HTTP response from
+// a go structure
 func WriteJSON(w http.ResponseWriter, data interface{}) {
 	jsonData, err := json.Marshal(data)
 	if err != nil {
@@ -14,6 +16,9 @@ func WriteJSON(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(jsonData)
 }
+
+// ReadJSON : convert a json from an HTTP request to
+// a go structure
 func ReadJSON(data interface{}, r *http.Request) error {
 	defer r.Body.Close()
 	return json.NewDecoder(r.Body).Decode(data)
