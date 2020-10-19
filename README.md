87f1cb2ba27e8e07cc468e30022bc35c23dc5e19
glenn_louarn <glenn.louarn@gmail.com>
2020-10-19 18:56:37 +0200

    CORS

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # go-rille
+
+
+go get -u github.com/swaggo/swag/cmd/swag
+
+go run server.go
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"github.com/swaggo/swag/example/basic/web"
-	"go-rille/internal/json"
-	"log"
+	"github.com/rs/cors"
 	"net/http"
 )
 
@@ -14,11 +12,34 @@ type person struct {
 }
 
 func tempHandler(w http.ResponseWriter, r *http.Request) {
-	p := person{Age: 12, Prenom: "Glenn", Name: "Louarn"}
-	json.WriteJSON(w, p)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte("{\"hello\": \"world\"}"))
+	})
+
+	// cors.Default() setup the middleware with default options being
+	// all origins accepted with simple methods (GET, POST). See
+	// documentation below for more options.
+	handler := cors.Default().Handler(mux)
+	http.ListenAndServe(":8080", handler)
+	/*p := person{Age: 12, Prenom: "Glenn", Name: "Louarn"}
+	json.WriteJSON(w, p)*/
 }
 
 func main() {
-	http.HandleFunc("/temp", tempHandler)
+	/*http.HandleFunc("/temp", tempHandler)
 	log.Fatal(http.ListenAndServe(":8888", nil))
+	*/
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte("{\"hello\": \"world\"}"))
+	})
+
+	// cors.Default() setup the middleware with default options being
+	// all origins accepted with simple methods (GET, POST). See
+	// documentation below for more options.
+	handler := cors.Default().Handler(mux)
+	http.ListenAndServe(":8081", handler)
 }
