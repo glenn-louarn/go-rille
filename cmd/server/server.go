fc736a0bb13cafd7ac7834bb741d6118f6a03fdb
glenn_louarn <glenn.louarn@gmail.com>
2020-10-20 14:24:56 +0200

    Get aeroport

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,35 +1,33 @@
 package main
 
 import (
-	"github.com/swaggo/swag/example/basic/web"
+	"github.com/rs/cors"
 	"go-rille/internal/json"
-	"log"
 	"net/http"
 )
 
-type person struct {
-	Age    int
-	Name   string
-	Prenom string
-}
-
-func tempHandler(w http.ResponseWriter, r *http.Request) {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		w.Write([]byte("{\"hello\": \"world\"}"))
-	})
-
-	// cors.Default() setup the middleware with default options being
-	// all origins accepted with simple methods (GET, POST). See
-	// documentation below for more options.
-	handler := cors.Default().Handler(mux)
-	http.ListenAndServe(":8080", handler)
-	/*p := person{Age: 12, Prenom: "Glenn", Name: "Louarn"}
-	json.WriteJSON(w, p)*/
+type aeroport struct {
+	Id              string
+	AeroportName    string
+	AeroportInitial string
+	Kmh             int
+	Temperature     int
+	Pluie           int
 }
 
 func main() {
-	http.HandleFunc("/temp", tempHandler)
-	log.Fatal(http.ListenAndServe(":8888", nil))
+	mux := http.NewServeMux()
+	aeroport1 := aeroport{
+		Id:              "15",
+		AeroportName:    "Charle de gaule",
+		AeroportInitial: "CDG",
+		Kmh:             20,
+		Temperature:     20,
+		Pluie:           20,
+	}
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		json.WriteJSON(w, aeroport1)
+	})
+	handler := cors.Default().Handler(mux)
+	http.ListenAndServe(":8081", handler)
 }
